a7eedbde130ee430193d70cbabb3312c52427059
Mitchell Hendee <email": null>
2017-08-04 14:45:10 -0500

    add helper method for celcius and fahrenheit conversion

--- public/js/application.js
+++ public/js/application.js
@@ -1,4 +1,10 @@
 var API_KEY="effcfe3f875caf621a210b8bc89b31f6";
+var cel = false;
+
+function displayTemp(fTemp, c){
+  if(c) return Math.round((fTemp - 32) * (5/9)); + " C";
+  return Math.round(fTemp) + " F";
+}
 
 $(document).ready(function() {
 
@@ -10,23 +16,28 @@ $(document).ready(function() {
     loc = d.loc.split(",");
     // Call to weather api
     $.getJSON('http://api.openweathermap.org/data/2.5/weather?units=imperial&lat=' +
-       loc[0] + '&lon='+ loc[1] + '&APPID=' + API_KEY,  function(wd){
-        console.log(wd);
-        var currentLocation = wd.name;
-        var currentWeather = wd.weather[0].description;
-        var currentTemp = wd.main.temp;
-        var high = wd.main.temp_max;
-        var low = wd.main.temp_min;
-        var icon = wd.main.icon;
+     loc[0] + '&lon='+ loc[1] + '&APPID=' + API_KEY,  function(apiData){
 
-        var iconUrl = 'http://openweathermap.org/img/w/' + icon + '.png';
+      var wd = apiData;
 
-        $('#currentLocation').html(currentLocation);
-        $('#currentTemp').html(currentTemp);
-        $('#currentWeather').html(currentWeather);
-        $('#high-low').html(high + '/' + low);
+      console.log(wd);
 
-      });
+      var currentLocation = wd.name;
+      var currentWeather = wd.weather[0].description;
+      var currentTemp = displayTemp(wd.main.temp, cel);
+      var high = displayTemp(wd.main.temp_max, cel);
+      var low = displayTemp(wd.main.temp_min, cel);
+      var icon = wd.weather[0].icon;
+
+      var iconUrl = 'http://openweathermap.org/img/w/' + icon + '.png';
+
+      $('#currentLocation').html(currentLocation);
+      $('#currentTemp').html(currentTemp);
+      $('#currentWeather').html(currentWeather);
+      $('#high-low').html(high + '/' + low);
+
+      $('#currentTemp').prepend('<img src=' + iconUrl + '>');
+    });
   });
 
 });
