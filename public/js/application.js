23a93ff11f856f8716190d1a1ebf0e0b45a5ec20
Mitchell Hendee <email": null>
2017-08-04 12:09:49 -0500

    able to get current location data and weather data tied to location on site load

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,5 @@
+var API_KEY="effcfe3f875caf621a210b8bc89b31f6";
+
 $(document).ready(function() {
 
   // get lat long
@@ -5,9 +7,12 @@ $(document).ready(function() {
 
   $.getJSON('https:/ipinfo.io', function(d){
     console.log("assigning location data");
-    loc = d.loc;
+    loc = d.loc.split(",");
     // Call to weather api
-
+    $.getJSON('http://api.openweathermap.org/data/2.5/weather?lat=' +
+       loc[0] + '&lon='+ loc[1] + '&APPID=' + API_KEY,  function(wd){
+        console.log(wd);
+      });
   });
 
 });
