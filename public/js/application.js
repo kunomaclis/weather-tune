12ec2848a59ad5176ac602a6658ca351b774d07a
Mitchell Hendee <email": null>
2017-08-04 14:22:28 -0500

    add jquery connections to update weather information

--- public/js/application.js
+++ public/js/application.js
@@ -9,9 +9,23 @@ $(document).ready(function() {
     console.log("assigning location data");
     loc = d.loc.split(",");
     // Call to weather api
-    $.getJSON('http://api.openweathermap.org/data/2.5/weather?lat=' +
+    $.getJSON('http://api.openweathermap.org/data/2.5/weather?units=imperial&lat=' +
        loc[0] + '&lon='+ loc[1] + '&APPID=' + API_KEY,  function(wd){
         console.log(wd);
+        var currentLocation = wd.name;
+        var currentWeather = wd.weather[0].description;
+        var currentTemp = wd.main.temp;
+        var high = wd.main.temp_max;
+        var low = wd.main.temp_min;
+        var icon = wd.main.icon;
+
+        var iconUrl = 'http://openweathermap.org/img/w/' + icon + '.png';
+
+        $('#currentLocation').html(currentLocation);
+        $('#currentTemp').html(currentTemp);
+        $('#currentWeather').html(currentWeather);
+        $('#high-low').html(high + '/' + low);
+
       });
   });
 
